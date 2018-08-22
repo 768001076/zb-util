324a8bc341e8d1337ce45f4b916e73f07f4b69dd
shijialei <shijialei>
2018-08-22 14:48:10 +0800

    zb-util-v1

--- src/main/java/test/myutil/MyUtilApplication.java
+++ src/main/java/test/myutil/MyUtilApplication.java
@@ -1,14 +1,10 @@
 package test.myutil;
 
-import hessian.MQServerMonitorHessianServer;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.remoting.caucho.HessianServiceExporter;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class})
 @ComponentScan(value = {"controller","util","service","config","aop","annotation","entity","datasource","hessian",
